f2b4e1ba729c6845ac3c0709d681c7118efccb2f
sahalkk <msahalkk357@gmail.com>
2023-08-11 12:50:52 +0530

    react-router-dom dependency

---
+++ Desktop/web/New/react crash/Routing/vite-project/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return <></>;
+}
+
+export default App;
